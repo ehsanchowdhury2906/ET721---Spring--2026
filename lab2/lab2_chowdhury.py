05a4aa02b94720a273326dd18fd4f927e35ec4f0
Ehsanchowdhury <Student@et.qcc.edu>
2026-01-29 13:28:51 -0500

    testing 2 file

--- lab2/lab2_chowdhury.py
+++ lab2/lab2_chowdhury.py
@@ -4,3 +4,9 @@ Lab 2: mastering Git workflow
 """
 username = input("enter a username: ")
 print(f"Welcome to Git commands {username}")
+
+# calculate the Hypotenusa
+x = 5
+y = 3
+hyp = (x**2 + y**2)**0.5
+print(f"The hypotenusa of sides {x} and {y} is {hyp:0.2f}")
