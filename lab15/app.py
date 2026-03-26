92237b649c20756bc204e530c508b474d0f50f23
ehsanchowdhury2906 <Chowdurrreee@gmail.com>
2026-03-26 20:28:50 +0000

    lab15

--- lab15/app.py
+++ lab15/app.py
@@ -1,49 +1,69 @@
 """
 Ehsan chowdhury
-March 24 2026
-lab 15: RESTful API and unit in a Flask app
+March 24, 2026
+lab 15: RESTful API and unit test in a Flask app
 """
 from flask import Flask, request, jsonify, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-#in-memory database (dictionary)
+# in-memory data
 items = {}
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-#create an item
+#CREATE an item
 @app.route('/items', methods =['POST'])
 def create_item():
-    #get_json method is used to read JSON data sent byt the client in a HTTP request
+    # get_json method is used to read JSON data sent by the client in an HTTP request
     data = request.get_json()
 
-    #generate a new unique ID for the new idem 
+    # generate a new unique id for the new item
     item_id = str(len(items)+1)
-    
-    #add the data collected for the new item
-    items[item_id]=data
 
-    #jsonfy converts a Python dictionary into a json response, and status as 201 (you request worked and a new resource was crerated)
+    # add the data collected for the new item
+    items[item_id] = data
+
+    # jsonify converts a python dictionary into a json response, and returns status code as 201 (your request worked and new resource was created)
     return jsonify({'id':item_id, 'item': data}), 201
 
-#READ ALL ITEMS
-@app.route('/items',methods = ['GET'])
+# READ ALL ITEMS
+@app.route('/items', methods = ['GET'])
 def get_items():
     return jsonify(items)
 
 #READ SINGLE ITEM
-@app.route('/items/<item_id>', methods=['GET'])
-def get_oneitem(item_id):
+@app.route('/items/<item_id>', methods = ['GET', 'PUT', 'DELETE'])
+def handle_item(item_id):
     item = items.get(item_id)
-    if not item:
-        #404 = server is reachable but the item you asked for doesnt exist
-        return jsonify({'Error:': "Item not found"}), 404
+
+    if request.method == 'GET':
+
+        if not item:
+            # 404 = server is reachable but the item you asked for doesn't exist
+            return jsonify({'Error':"Item no found"}), 404
     
-    return jsonify(item)
+        return jsonify(item)
 
+    # UPDATE
+    elif request.method == "PUT":
+        if not item:
+            return render_template('error.html', message = "ITEM NOT FOUND", item = item), 404
+        
+        data = request.get_json()
+        if not data:
+            return jsonify({'error' : 'Invalid input'}), 400
+        
+        items[item_id] = data
+        return render_template('update.html', item_id = item_id, item = data)
 
-if __name__=='__main__':
+    #DELETE
+    elif request.method == "DELETE":
+        deleted_item = items.pop(item_id)
+        return render_template('delete.html', item_id = item_id, deleted_item = deleted_item)
+    
+
+if __name__ == '__main__':
     app.run(debug=True)
